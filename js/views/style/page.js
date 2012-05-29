8a286ffffbb82fa4a29c5b43ea83358fae3694f2
Karl Westin <karl.westin@gmail.com>
2012-05-29 14:14:45 -0400

    Added a special "color" template
    That can be used by adding the tag:
    template:color (no space!!!)
    Templates are added to the template object in js/views/style/page.js
    It's desirable to move this somewhere else i think
    
    Also, for practicality i've added a user.css file

--- js/views/style/page.js
+++ js/views/style/page.js
@@ -5,23 +5,22 @@ define([
   'text!templates/style/page.html',
   'jscssp',
   'config',
-  'text!templates/plugins/plugin-template-example.html'
-], function($, _, Backbone, stylePageTemplate, jscssp, config, pluginTemplate){
+  'text!templates/plugins/plugin-template-example.html',
+  'text!templates/plugins/plugin-color.html'
+], function($, _, Backbone, stylePageTemplate, jscssp, config, pluginTemplate, colorTemplate){
   var StylePage = Backbone.View.extend({
     el: '.style-page',
     render: function () {
       var that = this;
       require(['text!'+ config.css_path + '/' + this.options.style], function (stylesheet){
-        var parser = new jscssp();
+        var parser = new jscssp(),
+            templates = {
+                standard: _.template(pluginTemplate),
+                color:    _.template(colorTemplate)
+            };
         stylesheet = parser.parse(stylesheet, false, true);
 
-        _.each(stylesheet.cssRules, function(rule) {
-          if(rule.type === 1) {
-            rule.template = _.template(pluginTemplate);
-          }
-        });
-
-        $(that.el).html(_.template(stylePageTemplate, {_:_, stylesheet: stylesheet}));
+        $(that.el).html(_.template(stylePageTemplate, {_:_, stylesheet: stylesheet, templates:templates}));
         _.each($('iframe'), function(iframe) {
           $(iframe).contents().find('body').html($(iframe).attr('data-content'));
           if($(iframe).hasClass('all-styles')) {
